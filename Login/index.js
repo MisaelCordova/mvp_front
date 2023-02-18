e4eef8da4fbef95781cea0e248eb858f9deb71e2
MisaelCordova <misael.cordova.f@gmail.com>
2023-02-17 21:23:55 -0300

    commit local

--- Login/index.js
+++ Login/index.js
@@ -1,18 +1,11 @@
 const url = "https://localhost:7230/Consumidor/Login"
 var documento = document.getElementById("cpfcnpj")
 var senha = document.querySelector("#password")
-
+let consumidor = {}
 const btnEntrar = document.querySelector("form")
-btnEntrar.addEventListener('submit', function(e){   
-     e.preventDefault()
-     let documentoDigitado;
-     if(documento.value.length == 14){
-        documentoDigitado = documento.value.slice(0,3) + documento.value.slice(4,7) + documento.value.slice(8,11) + documento.value.slice(12,14)
-     }else{
-        documentoDigitado = documento.value.slice(0,2) + documento.value.slice(3,6) + documento.value.slice(7,10) + documento.value.slice(11,15) + documento.value.slice(16,18)
-     } 
-    console.log(url + "/" + documentoDigitado)
+let documentoDigitado;
 
+function postBanco(){
     let request = fetch(url + "/" + documentoDigitado,{
         method: 'POST',
         headers: {
@@ -25,15 +18,36 @@ btnEntrar.addEventListener('submit', function(e){
         })
         
     })
-    console.log(request)
     request.then(function(res){
-        if(res.status == 200){
-            window.location.href="../PaginaInicialLogado/index.html"
-        }else if (res.status == 404){
-            alert("Documento não cadastrado")
-        }else if(res.status == 401){
-            alert("Senha Incorreta")
-        }
+        res.json().then(function(cons){
+            if(res.status == 200){
+                consumidor = cons
+               // console.log(consumidor)
+              window.location.href="../PaginaInicialLogado/index.html"
+            }else if (res.status == 404){
+                alert("Documento não cadastrado")
+            }else if(res.status == 401){
+                alert("Senha Incorreta")
+            }
+            
+        })
+        
     })
-
+}
+btnEntrar.addEventListener('submit', function(e){   
+     e.preventDefault()
+     
+     if(documento.value.length == 14){
+        documentoDigitado = documento.value.slice(0,3) + documento.value.slice(4,7) + documento.value.slice(8,11) + documento.value.slice(12,14)
+     }else{
+        documentoDigitado = documento.value.slice(0,2) + documento.value.slice(3,6) + documento.value.slice(7,10) + documento.value.slice(11,15) + documento.value.slice(16,18)
+     } 
+     postBanco();
+    
+    
+    
 })
+function teste(){
+    return consumidor
+}
+console.log(teste())
